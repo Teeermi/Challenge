789a572310f49f68f8fd2537a51bed1021e01543
Teeermi <145279497+Teeermi@users.noreply.github.com>
2023-12-27 22:47:47 +0100

    EqualSumCheck

---
+++ src/Ch7.java
@@ -0,0 +1,13 @@
+public class Ch7 {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasEqualSum(int jeden, int dwa, int trzy) {
+        int wynik = jeden + dwa;
+        if (wynik == trzy) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
